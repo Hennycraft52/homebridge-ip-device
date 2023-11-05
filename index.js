f7f97da7ab66f8d2d8bc1089f8eb9a577ef28e4b
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-11-05 15:06:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const IPDevicePlatform = require('./lib/IPDevicePlatform');
 
 module.exports = (api) => {
-  api.registerPlatform('homebridge-ip-device', 'IPDevicePlatform', IPDevicePlatform);
+  api.registerPlatform('homebridge-my-camera-plugin', 'IPDevicePlatform', IPDevicePlatform);
 };
